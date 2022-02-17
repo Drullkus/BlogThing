542bf257996864193040941b22ccfa5bf834fe1f
Tamaized <9671313+Tamaized@users.noreply.github.com>
2022-02-16 21:20:18 -0500

    use index not blank

--- src/main/java/us/drullk/blog/ApplicationController.java
+++ src/main/java/us/drullk/blog/ApplicationController.java
@@ -31,7 +31,7 @@ public class ApplicationController {
 		this.userService = userService;
 	}
 
-	@RequestMapping({"", "/", "/register", "/login", "/profile/{id}", "post/{id}"})
+	@RequestMapping({"index", "/", "/register", "/login", "/profile/{id}", "post/{id}"})
 	public String index(HttpServletRequest request, HttpServletResponse response) {
 		if (getSessionUser(request, userService).isEmpty())
 			killSession(response, userService, Optional.empty()); // Nuke the cookie if the session is invalid
